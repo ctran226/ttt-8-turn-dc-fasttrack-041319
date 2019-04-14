a9518668e6c9f9db73945f328858545e234a3292
C Tran <ctran226@yahoo.com>
2019-04-14 15:53:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def turn(board)
   if valid_move?(board, index)
     move(board, index, "X")
     display_board(board)
-    
+    turn(board)
   else
     while !valid_move?(board, index)
       turn(board)
